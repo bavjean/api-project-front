27cb087d3471114ae2651cd76c331a1dac818e2c
Brian Avjean <bavjean@gmail.com>
2020-06-07 23:10:14 -0400

    called api and began adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route } from "react-router-dom"
+import Header from "./Header"
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      joke: []
+    }
+  }
+
+  componentDidMount() {
+    let url =
+      "https://whispering-meadow-11234.herokuapp.com/random";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ joke: data });
+      });
+  }
+
+  render() {
+    console.log(this.state)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Header /> */}
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Chuck Norris</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/Random">Random Joke</Nav.Link>
+            <Nav.Link href="/Create">Create</Nav.Link>
+            <Nav.Link href="/Read">Read</Nav.Link>
+            <Nav.Link href="/Update">Update</Nav.Link>
+            <Nav.Link href="/Delete">Delete</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
   );
+  }
 }
 
 export default App;
