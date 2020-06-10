8c196326eae810a3a2171b7895f66cdf03e09f4b
Brian Avjean <bavjean@gmail.com>
2020-06-09 22:24:58 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# API Project Front-end
+# Chuck Norris API Project Front-end
+
+Built out a react front end to go with my previous [API project](https://github.com/bavjean/api-project). It interacts with and controls the data in the API that I built, with full CRUD through the interface.
+
+## Getting Started
+
+1. Check out the live page [here](https://hardcore-darwin-785938.netlify.app/) OR
+2. Fork and clone the repository
+3. Change to new directory
+4. Install dependencies
+5. Run locally
+
+
+## Built With
+
+1. React
+2. Bootstrap
+3. Formik
+4. Axios
+
+## Contributing
+
+Feel free to contribute via submitting an issue or doing a pull request. Any and all feedback is apreciated.
+
+## Author
+
+Brian Avjean
+
--- src/Create.js
+++ src/Create.js
@@ -1,55 +1,59 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import { useFormik } from 'formik'
+import { useFormik } from "formik";
 
 export default function Create() {
-    const axios = require('axios')
-    
-    const formik = useFormik({
-        initialValues: {
-          id: '',
-          joke: ''
-        },
-        onSubmit: values => {
-          alert(JSON.stringify(values, null, 2));
-          axios({
-            method: 'post',
-            url: 'https://whispering-meadow-11234.herokuapp.com/jokes',
-            data: {
-              id: values.id,
-              joke: values.joke
-            }
-          });
+  const axios = require("axios");
+
+  const formik = useFormik({
+    initialValues: {
+      id: "",
+      joke: "",
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      axios({
+        method: "post",
+        url: "https://whispering-meadow-11234.herokuapp.com/jokes",
+        data: {
+          id: values.id,
+          joke: values.joke,
         },
       });
+    },
+  });
 
   return (
-      <div className="container">
-        <Form onSubmit={formik.handleSubmit}>
-          <Form.Group controlId="id">
-            <Form.Label>Id</Form.Label>
-            <Form.Control 
-            type="text" 
-            placeholder="ID number" 
+    <div className="container">
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group controlId="id">
+          <Form.Label>Id</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Id number"
             onChange={formik.handleChange}
             value={formik.values.id}
-            />
-          </Form.Group>
+          />
+          <Form.Text className="text-muted">
+            Id number must not already be in use.
+          </Form.Text>
+        </Form.Group>
 
-          <Form.Group controlId="joke">
-            <Form.Label>Joke</Form.Label>
-            <Form.Control 
-            as="textarea" 
+        <Form.Group controlId="joke">
+          <Form.Label>Joke</Form.Label>
+          <Form.Control
+            as="textarea"
             rows="3"
             placeholder="Enter joke"
             onChange={formik.handleChange}
-            value={formik.values.joke}/>
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Create
-          </Button>
-        </Form>
-      </div>
+            value={formik.values.joke}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Create
+        </Button>
+      </Form>
+    </div>
   );
 }
